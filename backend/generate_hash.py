dd8c099557812b0983927418e10ee9e4336b8b01
Selva-Rubavathy <m.selvarubavathy@gmail.com>
2026-07-15 11:36:21 +0530

    Delete backend directory

--- backend/generate_hash.py
+++
@@ -1,6 +0,0 @@
-from app.auth import hash_password
-
-password = input("Enter the admin password you want to set: ")
-hashed = hash_password(password)
-print("\nHashed password (copy this):")
-print(hashed)
